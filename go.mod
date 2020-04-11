84be367c444bf2f72b780bf6353eba6658089138
Ryo Ota <nwtgck@nwtgck.org>
2020-04-11 19:41:18 +0900

    add --server flag to allow user to specify Piping Server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nwtgck/go-piping-duplex
 
 go 1.14
+
+require github.com/spf13/cobra v0.0.5
--- main/main.go
+++ main/main.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/nwtgck/go-piping-duplex"
+	"github.com/nwtgck/go-piping-duplex/cmd"
 	"os"
 )
 
 func main() {
-	// TODO: hard code
-	server := "https://ppng.io"
-	selfId := os.Args[1]
-	peerId := os.Args[2]
-	_, _ = fmt.Fprintf(os.Stderr, "[INFO] Server: %s\n", server)
-	_, _ = fmt.Fprintf(os.Stderr, "[INFO] Establishing between '%s' and '%s'...\n", selfId, peerId)
-	err := piping_duplex.Wait(server, selfId, peerId)
-	if err != nil {
-		panic(err)
-	}
-	_, _ = fmt.Fprintln(os.Stderr, "[INFO] Established!")
-	input := os.Stdin
-	output := os.Stdout
-	err = piping_duplex.Duplex(server, selfId, peerId, input, output)
-	if err != nil {
-		panic(err)
+	if err := cmd.RootCmd.Execute(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(-1)
 	}
 }
