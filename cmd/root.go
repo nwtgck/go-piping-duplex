68d1b141d151a296e6b304ce437f265fedf69601
Ryo Ota <nwtgck@nwtgck.org>
2020-04-12 22:06:40 +0900

    feat: add --symmetric option for symmetric key encryption with OpenPGP

--- cmd/root.go
+++ cmd/root.go
@@ -2,9 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/mattn/go-tty"
 	"github.com/nwtgck/go-piping-duplex"
+	"github.com/nwtgck/go-piping-duplex/util"
 	"github.com/nwtgck/go-piping-duplex/version"
 	"github.com/spf13/cobra"
+	"io"
 	"os"
 )
 
@@ -14,6 +17,7 @@ const (
 
 var server string
 var showsVersion bool
+var usesPassphrase bool
 
 func init() {
 	cobra.OnInitialize()
@@ -22,6 +26,7 @@ func init() {
 		defaultServer = "https://ppng.io"
 	}
 	RootCmd.Flags().StringVarP(&server,  "server",  "s", defaultServer, "Piping Server URL")
+	RootCmd.Flags().BoolVarP(&usesPassphrase, "symmetric", "c", false, "use symmetric passphrase protection")
 	RootCmd.Flags().BoolVarP(&showsVersion, "version", "v", false, "show version")
 }
 
@@ -37,6 +42,21 @@ var RootCmd = &cobra.Command{
 		if len(args) != 2 {
 			return fmt.Errorf("Your ID and peer ID are required\n")
 		}
+		var passphrase string
+		if usesPassphrase {
+			tty, err := tty.Open()
+			if err != nil {
+				return err
+			}
+			defer tty.Close()
+			fmt.Fprint(tty.Output(), "Passphrase: ")
+			passphrase, err = tty.ReadPasswordNoEcho()
+			if err != nil {
+				return err
+			}
+			fmt.Fprintln(tty.Output(), "[INFO] End-to-end encrypted")
+		}
+		var _ = passphrase
 		selfId := args[0]
 		peerId := args[1]
 		_, _ = fmt.Fprintf(os.Stderr, "[INFO] Server: %s\n", server)
@@ -46,8 +66,23 @@ var RootCmd = &cobra.Command{
 			return err
 		}
 		_, _ = fmt.Fprintln(os.Stderr, "[INFO] Established!")
-		input := os.Stdin
+		var input io.Reader = os.Stdin
+		if usesPassphrase {
+			input = util.OpenpgpSymmetricallyEncrypt(input, []byte(passphrase))
+		}
 		output := os.Stdout
-		return piping_duplex.Duplex(server, selfId, peerId, input, output)
+		r, err := piping_duplex.Duplex(server, selfId, peerId, input)
+		if err != nil {
+			return err
+		}
+		if usesPassphrase {
+			var decrypted, err = util.OpenpgpSymmetricallyDecrypt(r, []byte(passphrase))
+			if err != nil {
+				return err
+			}
+			r = decrypted
+		}
+		io.Copy(output, r)
+		return nil
 	},
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/nwtgck/go-piping-duplex
 
 go 1.14
 
-require github.com/spf13/cobra v0.0.5
+require (
+	github.com/mattn/go-tty v0.0.3
+	github.com/spf13/cobra v0.0.5
+	golang.org/x/crypto v0.0.0-20181203042331-505ab145d0a9
+)
